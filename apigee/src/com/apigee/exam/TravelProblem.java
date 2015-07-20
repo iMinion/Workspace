4d870172ab32867caa65117cd4da57029d1d4fdc
iMinion <k.srirammanoj@gmail.com>
2015-07-20 12:35:03 +0530

    Added Methods to perform necessary actions

--- apigee/src/com/apigee/exam/TravelProblem.java
+++ apigee/src/com/apigee/exam/TravelProblem.java
@@ -13,6 +13,9 @@ public class TravelProblem {
 			dist[i] = Integer.parseInt(str[1]);
 		}
 		sc.close();
+		TP t = new TP(n, litres, dist);
+		System.out.println(t.getNearestStation());
+		System.out.println(t.getAllStations());
 	}
 }
 
@@ -20,14 +23,94 @@ class TP {
 	int n;
 	int[] litres;
 	int[] dist;
+	boolean[] allTraversed;
+	MB m;
 	
 	TP(int n, int[] litres, int[] dist) {
 		this.n = n;
 		this.litres = litres;
 		this.dist = dist;
+		allTraversed = new boolean[n];
 	}
 	
+	public int getNearestStation() {
+		for(int i = 0; i < n ; i++) {
+			m = new MB();
+			if(litres[i] > dist[i]) {
+				for(int j = 0; j < n; j++) allTraversed[j] = false;
+				if(isRoutePossible(i)) return i;
+			}
+		}
+		return -1;
+	}
 	
+	public int getAllStations() {
+		int count = 0;
+		for(int i = 0; i < n ; i++) {
+			m = new MB();
+			if(litres[i] > dist[i]) {
+				for(int j = 0; j < n; j++) allTraversed[j] = false;
+				if(isRoutePossible(i)) count++;
+			}
+		}
+		return count;
+	}
+	
+	public boolean isRoutePossible(int i) {
+		int start = i;
+		for(; i < n; i++) {
+			if(m.getLitLeft() + litres[i] > dist[i]) {
+				m.setLit(m.getLitLeft() + litres[i] - dist[i]);
+				allTraversed[i] = true;
+			}
+			else {
+				return false;
+			}
+		}
+		if(i >= n) i -= 1;
+		if(!allTraversed()) {
+			for(; i > 0; i--) {
+				if(m.getLitLeft() + litres[i] > dist[i]) {
+					m.setLit(m.getLitLeft() + litres[i] - dist[i]);
+					allTraversed[i] = true;
+				}
+				else {
+					return false;
+				}
+			}
+		}
+		if (i <= 0) i += 1;
+		if( i != start) {
+			if( i > start) {
+				for(; i != start; i--) {
+					if(m.getLitLeft() + litres[i] > dist[i]) {
+						m.setLit(m.getLitLeft() + litres[i] - dist[i]);
+					}
+					else {
+						return false;
+					}					
+				}
+			}
+			if( i < start) {
+				for(; i != start; i++) {
+					if(m.getLitLeft() + litres[i] > dist[i]) {
+						m.setLit(m.getLitLeft() + litres[i] - dist[i]);
+					}
+					else {
+						return false;
+					}					
+				}
+			}
+		}
+		return true;
+	}
+	
+	public boolean allTraversed() {
+		for(int i = 0; i < n; i++) {
+			if(!allTraversed[i]) return false;
+		}
+		return true;
+	}
 }
 
 class MB {
