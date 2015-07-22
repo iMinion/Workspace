3d131128309ad45f49fcb54d1885c6fd49dad7b7
iMinion <k.srirammanoj@gmail.com>
2015-07-22 14:02:18 +0530

    Factorial Method is implemented

--- HackerRank/src/com/Warmup/ExtraLongFactorials.java
+++ HackerRank/src/com/Warmup/ExtraLongFactorials.java
@@ -1,5 +1,19 @@
 package com.Warmup;
 
-public class ExtraLongFactorials {
+import java.math.BigInteger;
 
+public class ExtraLongFactorials {
+	public static void main(String[] args) {
+        factorial(7);
+    }
+    
+    public static void factorial(int n) {
+        BigInteger[] fact = new BigInteger[n + 1];
+        fact[0] = new BigInteger("1");
+        fact[1] = new BigInteger("1");
+        for(int i = 2; i < fact.length; i++) {
+            fact[i] = fact[i-1].multiply(new BigInteger(Integer.toString(i)));
+        }
+        System.out.println(fact[fact.length - 1]);
+    }
 }
